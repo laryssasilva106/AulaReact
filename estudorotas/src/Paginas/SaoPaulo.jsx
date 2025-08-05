bd82f9c2cb13b1af1dee81dfdb32b3b468e79d2f
laryssasilva106 <laryssa.silva106@etec.sp.gov.br>
2025-08-05 14:41:55 -0300

    05/08/2025

---
+++ estudorotas/src/Paginas/SaoPaulo.jsx
@@ -0,0 +1,20 @@
+export default function SaoPaulo ()
+{
+    return(
+        <div>
+
+        <h1> Seja bem vindo ao Estado de São Paulo </h1>
+        <p>
+        O estado de São Paulo é o mais rico e populoso do Brasil, conhecido por sua forte economia e diversidade cultural. A capital, também chamada São Paulo, é um importante centro financeiro e cultural da América Latina. Além disso, o estado abriga belezas naturais, como a Serra da Cantareira e o litoral paulista.
+        </p>
+        <p>
+            <img scr=""/>
+        </p>
+        <p>
+            <a href="/">Voltar para Home</a>
+        </p>
+    
+
+        </div>
+    )
+}
